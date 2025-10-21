7a046b1f98f46577775a3d9fd49693bd32b58c67
nlanhduy <duyzhii@gmail.com>
2025-10-21 08:46:33 +0700

    feat: implement security for logger so as not to log sensitive field

--- src/logger/logging.interceptor.ts
+++ src/logger/logging.interceptor.ts
@@ -1,8 +1,10 @@
-import { WINSTON_MODULE_PROVIDER } from 'nest-winston'
-import { catchError, Observable, tap } from 'rxjs'
-import { Logger } from 'winston'
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable prettier/prettier */
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { catchError, Observable, tap } from 'rxjs';
+import { Logger } from 'winston';
 
-import { CallHandler, ExecutionContext, Inject, Injectable, NestInterceptor } from '@nestjs/common'
+import { CallHandler, ExecutionContext, Inject, Injectable, NestInterceptor } from '@nestjs/common';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
@@ -17,25 +19,62 @@ export class LoggingInterceptor implements NestInterceptor {
     const { method, originalUrl, params, query, body } = req
     const start = Date.now()
 
+    // Sanitize sensitive fields before logging
+    const sanitizedBody = this.sanitize(body)
+    const sanitizedParams = this.sanitize(params)
+    const sanitizedQuery = this.sanitize(query)
+
     this.logger.info(`Incoming Request: ${method} ${originalUrl}`, {
-      params,
-      query,
-      body,
+      params: sanitizedParams,
+      query: sanitizedQuery,
+      body: sanitizedBody,
     })
 
     return next.handle().pipe(
       tap(() => {
         const duration = Date.now() - start
-        this.logger.info(`Response: ${method} ${originalUrl} ${res.statusCode} - ${duration}ms`)
+        this.logger.info(
+          `Response: ${method} ${originalUrl} ${res.statusCode} - ${duration}ms`
+        )
       }),
       catchError((err) => {
         const duration = Date.now() - start
         const statusCode = err.status || 500
-        this.logger.error(`Error: ${method} ${originalUrl} ${statusCode} - ${duration}ms`, {
-          error: err.message,
-        })
+        this.logger.error(
+          `Error: ${method} ${originalUrl} ${statusCode} - ${duration}ms`,
+          { error: err.message }
+        )
         throw err
       })
     )
+  }
+
+  /**
+   * Redacts sensitive fields from an object
+   */
+  private sanitize(obj: Record<string, any> = {}): Record<string, any> {
+    const sensitiveKeys = [
+      'password',
+      'token',
+      'accessToken',
+      'refreshToken',
+      'authorization',
+      'secret',
+      'apiKey',
+    ]
+
+    const clone: Record<string, any> = {}
+    for (const key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        if (sensitiveKeys.includes(key.toLowerCase())) {
+          clone[key] = '[REDACTED]'
+        } else if (typeof obj[key] === 'object' && obj[key] !== null) {
+          clone[key] = this.sanitize(obj[key]) // recursively sanitize
+        } else {
+          clone[key] = obj[key]
+        }
+      }
+    }
+    return clone
   }
 }
